a02d5e3f735ffb1b457e334613efa2c9feebc8da
Mike <kdx7214@gmail.com>
2022-09-06 18:17:12 -0500

    test

---
+++ DVDReadTest/Program.cs
@@ -0,0 +1,200 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+
+
+using DVDUtilities;
+using MediaInfoLib;
+
+NEW_VMG vmg = new NEW_VMG(@"E:\VIDEO_TS\VIDEO_TS.IFO");
+
+
+
+
+
+Environment.Exit(0);
+
+
+
+
+
+
+
+
+
+
+var d = new DVDInfo("E:");
+
+foreach (var title in d.titles)
+{
+    Console.WriteLine("Title # {0}", title.Id + 1);
+
+    foreach (var audio_track in title.AudioTracks)
+    {
+        string s = String.Format("\tAudio track # {0}:  {1}", audio_track.Id + 1, audio_track.Language);
+        s += String.Format(" ({0})", audio_track.CodingMode.ToString());
+        s += String.Format(" ({0} ch)", audio_track.Channels.ToString());
+        //if (audio_track.DolbySurround)
+        //    s += " (Dolby Surround)";
+        Console.WriteLine(s);
+    }
+
+    foreach (var sub_track in title.SubtitleTracks)
+    {
+        Console.WriteLine(String.Format("\tSubtitle track [{0}],  {1}", sub_track.Id, sub_track.SubtitleDescription));
+    }
+
+}
+
+Environment.Exit(0);
+
+/*
+ * 
+ * MediaInfo has 99% of the information we need to parse from the disk.  The ONLY thing
+ * missing is the relationship between titles and video title sets.  That means the VIDEO_TS.IFO
+ * file must be parsed manually to get the title/vts relationships from TT_SRPT.  The VTS # from the
+ * IFO can be used to generate the filenames we need to parse with MediaInfo.
+ * 
+ */
+
+
+
+MediaInfo mi = new MediaInfo();
+Console.WriteLine(mi.Option("Info_version"));
+
+
+/*
+ * Process the IFO and display
+ */
+
+
+mi.Open(@"e:\video_ts\vts_01_0.ifo");
+Console.WriteLine("\n\nData for:  VTS_01_0.IFO");
+Console.WriteLine(String.Format("    Length in ms:  {0}", mi.Get(StreamKind.General, 0, "Duration")));
+Console.WriteLine(String.Format("    Video Count: [{0}], Audio Count: [{1}], Subtitle Count: [{2}]",
+                    mi.Get(StreamKind.General, 0, "VideoCount"),
+                    mi.Get(StreamKind.General, 0, "AudioCount"),
+                    mi.Get(StreamKind.General, 0, "TextCount")));
+Console.WriteLine();
+
+for (int i = 0; i < mi.Count_Get(StreamKind.Video); i++)
+{
+    Console.WriteLine(String.Format("    Video [{0}]: ID[0x{1}], Format[{2}:{3}], Size[{4}x{5}], [{6}], [{7}], [{8} fps]", i, 
+        Int32.Parse(mi.Get(StreamKind.Video, 0, "ID")).ToString("X"),
+        mi.Get(StreamKind.Video, i, "Format"),
+        mi.Get(StreamKind.Video, i, "Format_Version"),
+        mi.Get(StreamKind.Video, i, "Width"),
+        mi.Get(StreamKind.Video, i, "Height"),
+        mi.Get(StreamKind.Video, i, "DisplayAspectRatio/String"),
+        mi.Get(StreamKind.Video, i, "Standard"),
+        mi.Get(StreamKind.Video, i, "FrameRate")   ));
+}
+Console.WriteLine();
+
+for (int i = 0; i < mi.Count_Get(StreamKind.Audio); i++)
+{
+    Console.Write(String.Format("    Audio [{0}]: ", i));
+    Console.Write(String.Format("ID[{0}]", mi.Get(StreamKind.Audio, i, "ID")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Audio, i, "Language/String")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Audio, i, "Format")));
+    Console.Write(String.Format(", [{0} ch]", mi.Get(StreamKind.Audio, i, "Channel(s)")));
+
+    string s = mi.Get(StreamKind.Audio, i, "Language_More");
+    if (s != String.Empty)
+        Console.Write(String.Format(", [{0}]", s));
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+for (int i = 0; i < mi.Count_Get(StreamKind.Text); i++)
+{
+    Console.Write(String.Format("    Subtitle [{0}]: ", i));
+    Console.Write(String.Format("ID[{0}]", mi.Get(StreamKind.Text, i, "ID")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Text, i, "Language/String")));
+    string s = mi.Get(StreamKind.Text, i, "Language_More");
+    if (s != String.Empty)
+        Console.Write(String.Format(", [{0}]", s));
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+mi.Close();
+
+
+/*
+ * Process the VOB and display.  Some fields are only available in the VOB
+ */
+
+
+// Remember, the VTS_01_0.VOB is the MENU video
+mi.Open(@"E:\VIDEO_TS\VTS_01_1.VOB");
+Console.WriteLine("Data for:  VTS_01_1.VOB");
+Console.WriteLine(String.Format("    Length in ms:  {0}", mi.Get(StreamKind.General, 0, "Duration")));
+Console.WriteLine(String.Format("    Video Count: [{0}], Audio Count: [{1}], Subtitle Count: [{2}]",
+                    mi.Get(StreamKind.General, 0, "VideoCount"),
+                    mi.Get(StreamKind.General, 0, "AudioCount"),
+                    mi.Get(StreamKind.General, 0, "TextCount")));
+Console.WriteLine();
+
+
+for (int i = 0; i < mi.Count_Get(StreamKind.Video); i++)
+{
+    Console.WriteLine(String.Format("    Video [{0}]: ID[0x{1}], Format[{2}:{3}], Size[{4}x{5}], [{6}], [{7} fps]", i,
+        Int32.Parse(mi.Get(StreamKind.Video, 0, "ID")).ToString("X"),
+        mi.Get(StreamKind.Video, i, "Format"),
+        mi.Get(StreamKind.Video, i, "Format_Version"),
+        mi.Get(StreamKind.Video, i, "Width"),
+        mi.Get(StreamKind.Video, i, "Height"),
+        mi.Get(StreamKind.Video, i, "DisplayAspectRatio/String"),
+        mi.Get(StreamKind.Video, i, "FrameRate")));
+}
+Console.WriteLine();
+
+
+int audio_count = mi.Count_Get(StreamKind.Audio);
+for (int i = 0; i < audio_count; i++)
+{
+    Console.Write(String.Format("    Audio [{0}]: ", i));
+    Console.Write(String.Format("ID[{0}]", mi.Get(StreamKind.Audio, i, "ID/String")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Audio, i, "Format")));
+    Console.Write(String.Format(", [{0} ch]", mi.Get(StreamKind.Audio, i, "Channel(s)")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Audio, i, "ChannelLayout")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Audio, i, "BitRate/String")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Audio, i, "BitRate_Mode")));
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+for (int i = 0; i < mi.Count_Get(StreamKind.Text); i++)
+{
+    Console.Write(String.Format("    Subtitle [{0}]: ", i));
+    Console.Write(String.Format("ID[{0}]", mi.Get(StreamKind.Text, i, "ID/String")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Text, i, "Format")));
+    Console.Write(String.Format(", [{0}]", mi.Get(StreamKind.Text, i, "MuxingMode")));
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+
+
+
+
+mi.Close();
+
+
+
+
+
+
+
+
+
+
+
---
+++ DVDUtilities/NewCode.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Buffers.Binary;
+
+// This is a test
+
+
+namespace DVDUtilities
+{
+    #region NEW_VMG
+    public class NEW_VMG
+    {
+        public string? Identifier { get; private set; }
+        public System.UInt32 LastSectorVMG { get; private set; }
+        public System.UInt32 LastSectorIFO { get; private set; }
+        public System.Decimal Version { get; private set; }
+        public System.UInt32 VMGCategory { get; private set; }
+        public System.UInt16 NumberOfVolumes { get; private set; }
+        public System.UInt16 VolumeNumber { get; private set; }
+        public System.Byte SideID { get; private set; }
+        public System.UInt16 NumberTitleSets { get; private set; }
+        public string? ProviderID { get; private set; }
+        public System.UInt64 VmgPos { get; private set; }
+        public System.UInt32 EndByteAddress_VMGI_MAT { get; private set; }
+        public System.UInt32 StartAddress_FP_PGC { get; private set; }
+        public System.UInt32 StartSector_Menu_VOB { get; private set; }
+        public System.UInt32 SectorPointer_TT_SRPT { get; private set; }
+        public System.UInt32 SectorPointer_VMGM_PGCI_UT { get; private set; }
+        public System.UInt32 SectorPointer_VMG_PTL_MAIT { get; private set; }
+        public System.UInt32 SectorPointer_VMG_VTS_ATRT { get; private set; }
+        public System.UInt32 SectorPointer_VMG_TXTDT_MG { get; private set; }
+        public System.UInt32 SectorPointer_VMGM_C_ADT { get; private set; }
+        public System.UInt32 SectorPointer_VMGM_VOBU_ADMAP { get; private set; }
+        public System.UInt16 VideoAttributes_VMGM_VOBS { get; private set; }
+        public System.UInt16 NumberAudioStreams_VMGM_VOBS { get; private set; }
+        public NewAudioAttributes[]? AudioAttributes_VMGM_VOBS { get; private set; }
+        public System.UInt16 NumberSubpictureStreams_VMGM_VOBS { get; private set; }
+        public NewSubpictureAttributes? SubpictureAttributes_VMGM_VOBS { get; private set; }
+
+
+        // In case the user already created the BinaryReader
+        public NEW_VMG(BinaryReader bin)
+        {
+            Init(bin);
+        }
+
+        // In case the user sends a filename
+        public NEW_VMG(string _path)
+        {
+            byte[] bytes = File.ReadAllBytes(_path);
+            using (var stream = new MemoryStream(bytes, 0, bytes.Length, false))
+            {
+                using (var bin = new BinaryReader(stream))
+                {
+                    Init(bin);
+                }
+            }
+        }
+
+
+        //
+        // DVD structures are big endian, Windows is little endian.  Get32 swaps the bytes
+
+        private void Init(BinaryReader bin)
+        {
+//            try
+            {
+                Identifier = new string(bin.ReadChars(12));
+                LastSectorVMG = bin.ReadUInt32();
+                bin.BaseStream.Position = 0x1C;
+                LastSectorIFO = bin.ReadUInt32();
+
+
+                var t = bin.ReadUInt16() >> 8;
+                string major = (t >> 4 & 15).ToString();
+                string minor = (t & 15).ToString();
+                Version = Decimal.Parse( major + "." + minor);
+
+                VMGCategory = reverse(bin.ReadUInt32());
+                NumberOfVolumes = reverse(bin.ReadUInt16());
+                VolumeNumber = reverse(bin.ReadUInt16());
+                SideID = bin.ReadByte();
+                bin.BaseStream.Position = 0x3E;
+                NumberTitleSets = reverse(bin.ReadUInt16());
+                ProviderID = new string(bin.ReadChars(32));
+                VmgPos = reverse(bin.ReadUInt64());
+                bin.BaseStream.Position = 0x80;
+                EndByteAddress_VMGI_MAT = reverse(bin.ReadUInt32());
+                StartAddress_FP_PGC = reverse(bin.ReadUInt32());
+                bin.BaseStream.Position = 0xC0;
+                StartSector_Menu_VOB = reverse(bin.ReadUInt32());
+                SectorPointer_TT_SRPT = reverse(bin.ReadUInt32());
+                SectorPointer_VMGM_PGCI_UT = reverse(bin.ReadUInt32());
+                SectorPointer_VMG_PTL_MAIT = reverse(bin.ReadUInt32());
+                bin.BaseStream.Position = 0xD0;
+                SectorPointer_VMG_VTS_ATRT = reverse(bin.ReadUInt32());
+                SectorPointer_VMG_TXTDT_MG = reverse(bin.ReadUInt32());
+                SectorPointer_VMGM_C_ADT = reverse(bin.ReadUInt32());
+                SectorPointer_VMGM_VOBU_ADMAP = reverse(bin.ReadUInt32());
+                bin.BaseStream.Position = 0x100;
+                VideoAttributes_VMGM_VOBS = reverse(bin.ReadUInt16());
+                NumberAudioStreams_VMGM_VOBS = reverse(bin.ReadUInt16());
+                AudioAttributes_VMGM_VOBS = new NewAudioAttributes[8];
+                for (int i = 0; i < 8; ++i)
+                    AudioAttributes_VMGM_VOBS[i] = new NewAudioAttributes(bin.ReadBytes(8));
+                bin.BaseStream.Position = 0x154;
+                NumberSubpictureStreams_VMGM_VOBS = reverse(bin.ReadUInt16());
+                SubpictureAttributes_VMGM_VOBS = new NewSubpictureAttributes(bin.ReadBytes(6));
+            }
+//            catch { }  // Don't do anything, but don't set values
+        }
+
+        private System.UInt16 reverse(System.UInt16 n) { return BinaryPrimitives.ReverseEndianness(n); }
+        private System.UInt32 reverse(System.UInt32 n) { return BinaryPrimitives.ReverseEndianness(n); }
+        private System.UInt64 reverse(System.UInt64 n) { return BinaryPrimitives.ReverseEndianness(n); }
+
+        ~NEW_VMG()
+        {
+            // Explicitly tell the GC we are done with this
+            if (Identifier != null) Identifier = null;
+            if (ProviderID != null) ProviderID = null;
+            if (AudioAttributes_VMGM_VOBS != null) AudioAttributes_VMGM_VOBS = null;
+            if (SubpictureAttributes_VMGM_VOBS != null) SubpictureAttributes_VMGM_VOBS = null;
+        }
+
+    }
+    #endregion
+
+    #region NewAudioAttributes
+    public class NewAudioAttributes
+    {
+        public byte[]? _data;
+
+        public NewAudioAttributes()
+        {
+            if (_data == null)
+                _data = new byte[8];
+        }
+
+        public NewAudioAttributes(byte[]? data)
+        {
+            _data = data;
+        }   
+
+        ~NewAudioAttributes()
+        {
+            // Explicitly tell the GC we are done with this
+            if (_data != null)
+                _data = null;
+        }
+
+    }
+    #endregion
+
+    #region NewSubpictureAttributes
+    public class NewSubpictureAttributes
+    {
+        public byte[]? _data;
+
+        public NewSubpictureAttributes()
+        {
+            if (_data == null)
+                _data = new byte[6];
+        }
+
+        public NewSubpictureAttributes(byte[]? data)
+        {
+            _data = data;
+        }
+
+        ~NewSubpictureAttributes()
+        {
+            // Explicitly tell the GC we are done with this
+            if (_data != null)
+                _data = null;
+        }
+
+
+    }
+    #endregion
+}
